03c1477363272d99e1065f195095008a8aa99743
전유진 <wjsdbwls7777@gmail.com>
2021-01-11 22:31:40 +0900

    react function or class type-함수에서 state사용법

--- src/App.js
+++ src/App.js
@@ -5,27 +5,38 @@ function App() {
   return (
     <div className="container">
       <h1>Hello World</h1>
-      <FuncComp></FuncComp>
-      <ClassComp></ClassComp>
+      <FuncComp initNumber={2}></FuncComp>
+      <ClassComp initNumber={2}></ClassComp>
     </div>
   );
 }
 
-function FuncComp() {
+// Hook을 이용하여 function style code에서도 state 사용이 가능해짐
+function FuncComp(props) {
   return (
     <div className="container">
       <h2>function style component</h2>
+      <p>Number: {props.initNumber}</p>
     </div>
   );
 }
 
 class ClassComp extends React.Component {
+  state = {
+    number: this.props.initNumber
+  }
   render() {
     return (
     <div className="container">
       <h2>class style component</h2>
+      <p>Number: {this.state.number}</p>
+      <input type="button" value="random" onClick={
+        function() {
+          this.setState({number: Math.random()});
+        }.bind(this)
+      }/>
     </div>
-  );
+    )
   }
 }
 export default App;
